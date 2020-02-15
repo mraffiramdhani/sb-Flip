6f52f352ffb67a69f60d84e761db81b1105afa26
mraffiramdhani <raffi.programming@gmail.com>
2020-02-15 12:43:51 +0700

    adding config file

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+	class Config {
+		public $DB_HOST = "localhost";
+		public $DB_USER = "root";
+		public $DB_PASS = "";
+		public $DB_NAME = "flip_test";
+		public $SECRET_KEY = "HyzioY7LP6ZoO7nTYKbG8O4ISkyWnX1JvAEVAhtWKZumooCzqp41";
+		public $API_URI = "https://nextar.flip.id/";
+	}
+
+?>
